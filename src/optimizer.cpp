62368f74a0fee996795a045a7731bc2a9fd9ed83
Dénes Fintha <denes.fintha@gmail.com>
2024-10-20 13:35:46 +0200

    Tidy up the optimizer and fix a copy-paste mishap in its comments

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -52,11 +52,143 @@ static std::optional<double> evaluate_child(const expr::node_ptr& child) {
     return std::nullopt;
 }
 
+static std::optional<expr::optimizer_result> make_optimized_addition(
+    expr::node_ptr& original
+) {
+    for (size_t i = 0; i < original->children.size(); ++i) {
+        const auto value = evaluate_child(original->children[i]);
+
+        // Addition of 0 is a no-op both ways.
+        if (value && is_near(*value, 0))
+            return std::move(original->children[1 - i]);
+    }
+    return std::nullopt;
+}
+
+static std::optional<expr::optimizer_result> make_optimized_subtraction(
+    expr::node_ptr& original,
+    const expr::location_t location
+) {
+    // Subtraction of a variable from itself results in 0.
+    if (are_binary_operands_the_same(original->children))
+        return expr::make_number_literal_node("0", location);
+
+    const auto value_0 = evaluate_child(original->children[0]);
+    const auto value_1 = evaluate_child(original->children[1]);
+
+    // Subtraction of 0 is a no-op.
+    if (value_1 && is_near(*value_1, 0))
+        return std::move(original->children[0]);
+
+    // Subtraction from 0 is a sign change.
+    if (value_0 && is_near(*value_0, 0)) {
+        return expr::make_unary_operator_node(
+            "-",
+            std::move(original->children[1]),
+            location
+        );
+    }
+
+    return std::nullopt;
+}
+
+static std::optional<expr::optimizer_result> make_optimized_multiplication(
+    expr::node_ptr& original,
+    const expr::location_t location
+) {
+    // Multiplication of a variable with itself is its 2nd power.
+    if (are_binary_operands_the_same(original->children)) {
+        return expr::make_binary_operator_node(
+            "^",
+            std::move(original->children[0]),
+            expr::make_number_literal_node(
+                "2",
+                original->children[1]->location
+            ),
+            location
+        );
+    }
+
+    for (size_t i = 0; i < original->children.size(); ++i) {
+        const auto value = evaluate_child(original->children[i]);
+
+        // Multiplication with 0 results in 0.
+        if (value && is_near(*value, 0))
+            return expr::make_number_literal_node("0", location);
+
+        // Multiplication with 1 is a no-op (both ways).
+        if (value && is_near(*value, 1))
+            return std::move(original->children[1 - i]);
+
+        // Multiplication with -1 is a sign change (both ways).
+        if (value && is_near(*value, -1)) {
+            return expr::make_unary_operator_node(
+                "-",
+                std::move(original->children[1 - i]),
+                location
+            );
+        }
+    }
+
+    return std::nullopt;
+}
+
+static std::optional<expr::optimizer_result> make_optimized_division(
+    expr::node_ptr& original,
+    const expr::location_t location
+) {
+    // Division of a variable with itself results in 1.
+    if (are_binary_operands_the_same(original->children)) {
+        return  expr::make_number_literal_node("1", location);
+    }
+
+    const auto value_0 = evaluate_child(original->children[0]);
+    const auto value_1 = evaluate_child(original->children[1]);
+
+    // Division of 0 is always 0.
+    if (value_0 && is_near(*value_0, 0))
+        return expr::make_number_literal_node("0", location);
+
+    // Division with 1 is a no-op.
+    if (value_1 && is_near(*value_1, 1))
+        return std::move(original->children[0]);
+
+    // Division with -1 is a sign change.
+    if (value_1 && is_near(*value_1, -1)) {
+        return expr::make_unary_operator_node(
+            "-",
+            std::move(original->children[0]),
+            location
+        );
+    }
+
+    return std::nullopt;
+}
+
+static std::optional<expr::optimizer_result> make_optimized_exponentiation(
+    expr::node_ptr& original,
+    const expr::location_t location
+) {
+    const auto value = evaluate_child(original->children[1]);
+
+    // The 0th power of every number is 1.
+    if (value && is_near(*value, 0))
+        return expr::make_number_literal_node("1", location);
+
+    // The 1st power of every number is itself.
+    if (value && is_near(*value, 1))
+        return std::move(original->children[0]);
+
+    return std::nullopt;
+}
+
 static expr::optimizer_result make_optimized_binary_op(
     const std::string& operation,
     std::vector<expr::node_ptr> children,
     expr::location_t location
 ) {
+    // We construct a node with the already-optimized children, that we can
+    // perform optimizations on.
     auto original = expr::node_ptr{
         new expr::node_t{
             expr::node_t::type_t::BINARY_OP,
@@ -66,7 +198,7 @@ static expr::optimizer_result make_optimized_binary_op(
         }
     };
 
-    // if every operand is a literal, the expression can be evaluated
+    // If every operand is a number, the expression can be evaluated parse-time.
     if (are_all_children_numbers(original->children)) {
         const auto value = expr::evaluate(original, {}, {});
         if (value) {
@@ -77,117 +209,42 @@ static expr::optimizer_result make_optimized_binary_op(
         }
     }
 
-    if (operation == "+") {
-        for (size_t i = 0; i < original->children.size(); ++i) {
-            const auto value = evaluate_child(original->children[i]);
+    // Otherwise, we'll perform various operation-specific checks to simplify
+    // the subexpression.
 
-            // addition with 0 is a no-op (both ways)
-            if (value && is_near(*value, 0))
-                return std::move(original->children[1 - i]);
-        }
+    if (operation == "+") {
+        if (auto optimized = make_optimized_addition(original))
+            return std::move(*optimized);
     }
 
     if (operation == "-") {
-        // division of a variable from itself results in 0
-        if (are_binary_operands_the_same(original->children)) {
-            return expr::make_number_literal_node("0", location);
-        }
-
-        const auto value_0 = evaluate_child(original->children[0]);
-        const auto value_1 = evaluate_child(original->children[1]);
-
-        // subtraction of 0 is a no-op
-        if (value_1 && is_near(*value_1, 0))
-            return std::move(original->children[0]);
-
-        // subtraction from 0 is a sign change
-        if (value_0 && is_near(*value_0, 0))
-            return expr::make_unary_operator_node(
-                "-",
-                std::move(original->children[1]),
-                location
-            );
+        if (auto optimized = make_optimized_subtraction(original, location))
+            return std::move(*optimized);
     }
 
     if (operation == "*") {
-        // multiplication of a variable with itself is its 2nd power
-        if (are_binary_operands_the_same(original->children)) {
-            return expr::make_binary_operator_node(
-                "^",
-                std::move(original->children[0]),
-                expr::make_number_literal_node(
-                    "2",
-                    original->children[1]->location
-                ),
-                location
-            );
-        }
-
-        for (size_t i = 0; i < original->children.size(); ++i) {
-            const auto value = evaluate_child(original->children[i]);
-
-            // multiplication with 0 results in 0
-            if (value && is_near(*value, 0))
-                return expr::make_number_literal_node("0", location);
-
-            // multiplication with 1 is a no-op (both ways)
-            if (value && is_near(*value, 1))
-                return std::move(original->children[1 - i]);
-
-            // multiplication with -1 is a sign change (both ways)
-            if (value && is_near(*value, -1)) {
-                return expr::make_unary_operator_node(
-                    "-",
-                    std::move(original->children[1 - i]),
-                    location
-                );
-            }
-        }
+        if (auto optimized = make_optimized_multiplication(original, location))
+            return std::move(*optimized);
     }
 
     if (operation == "/") {
-        // division of a variable with itself results in 1
-        if (are_binary_operands_the_same(original->children)) {
-            return  expr::make_number_literal_node("1", location);
-        }
-
-        const auto value_0 = evaluate_child(original->children[0]);
-        const auto value_1 = evaluate_child(original->children[1]);
-
-        // division of 0 is always 0
-        if (value_0 && is_near(*value_0, 0))
-            return expr::make_number_literal_node("0", location);
-
-        // division with 1 is a no-op
-        if (value_1 && is_near(*value_1, 1))
-            return std::move(original->children[0]);
-
-        // division with -1 is a sign change
-        if (value_1 && is_near(*value_1, -1)) {
-            return expr::make_unary_operator_node(
-                "-",
-                std::move(original->children[0]),
-                location
-            );
-        }
+        if (auto optimized = make_optimized_division(original, location))
+            return std::move(*optimized);
     }
 
     if (operation == "^") {
-        const auto value = evaluate_child(original->children[1]);
-
-        // the 0th power of every number is 1
-        if (value && is_near(*value, 0))
-            return expr::make_number_literal_node("1", location);
-
-        // the 1st power of every number is itself
-        if (value && is_near(*value, 1))
-            return std::move(original->children[0]);
+        if (auto optimized = make_optimized_exponentiation(original, location))
+            return std::move(*optimized);
     }
 
+    // If we could not perform any optimizations, we return with a node with
+    // optimized children.
     return original;
 }
 
 expr::optimizer_result expr::optimize(const expr::node_ptr& root) {
+    // First, we optimize all the children of the node so we can perform later
+    // checks on the simplest equivalent subexpression.
     std::vector<expr::node_ptr> children;
     for (auto& child : root->children) {
         if (auto optimized = expr::optimize(child)) {
@@ -209,6 +266,8 @@ expr::optimizer_result expr::optimize(const expr::node_ptr& root) {
         );
     }
 
+    // Unary operations on simple numbers are either no-op or a sign change.
+    // Both can be evaluated during parse-time.
     if (root->type == expr::node_t::type_t::UNARY_OP) {
         if (root->children[0]->type == expr::node_t::type_t::NUMBER) {
             if (const auto value = expr::evaluate(root, {}, {})) {
@@ -220,6 +279,8 @@ expr::optimizer_result expr::optimize(const expr::node_ptr& root) {
         }
     }
 
+    // Even if no optimization was done, the children of the node were
+    // optimized.
     return expr::node_ptr{
         new expr::node_t{
             root->type,
